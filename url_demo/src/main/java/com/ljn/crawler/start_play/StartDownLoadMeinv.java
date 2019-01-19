7ddd828368c5f57655595fd8b70cd116fa3788ea
ljn_mnpp <137189863@qq.com>
2019-01-19 23:26:27 +0800

    httpClient 测试

--- url_demo/src/main/java/com/ljn/crawler/start_play/StartDownLoadMeinv.java
+++ url_demo/src/main/java/com/ljn/crawler/start_play/StartDownLoadMeinv.java
@@ -7,7 +7,7 @@ import com.ljn.crawler.meinv.Display;
  * @Date 2019/1/13 19:43
  * @Description:
  **/
-public class StartDownMeinv {
+public class StartDownLoadMeinv {
     public static void main(String[] args) {
         Display.start();
     }
